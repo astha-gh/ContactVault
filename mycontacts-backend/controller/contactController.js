bcd4109a07064653e0ca4a5ad186f59d9a1453cb
astha-gh <asthadevadiga7@gmail.com>
2025-06-13 11:54:21 +0530

    Initial Commit

---
+++ mycontacts-backend/controller/contactController.js
@@ -0,0 +1,84 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
+
+//@description Get all contact
+//@route GET /api/contacts
+//@access private
+const getContacts = asyncHandler(async (req, res) => {
+    const contacts = await Contact.find({ user_id: req.user.id });
+    res.status(200).json(contacts);
+});
+
+//@description Create a contact
+//@route POST /api/contact
+//@access private
+const createContact = asyncHandler(async (req, res) => {
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const contact = await Contact.create({
+        name,
+        email,
+        phone,
+        user_id: req.user.id
+    });
+    res.status(201).json(contact);
+});
+
+//@description Get a contact
+//@route GET /api/contact/:id
+//@access private
+const getContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User does not have permission to view this contact");
+    }
+    res.status(200).json(contact);
+});
+
+//@description Update a contact
+//@route PUT /api/contact/:id
+//@access private
+const updateContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User does not have permission to update this contact");
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+    res.status(200).json(updatedContact);
+});
+
+//@description Delete a contact
+//@route DELETE /api/contacts/:id
+//@access private
+const deleteContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User does not have permission to delete this contact");
+    }
+    await Contact.deleteOne({ _id: req.params.id });
+    res.status(200).json({ message: "Contact deleted successfully" });
+});
+
+module.exports = { getContacts, createContact, getContact, updateContact, deleteContact };
---
+++ mycontacts-backend/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require("cors");
+const errorHandler = require('./middleware/errorHandler');
+const Connectdb = require('./config/dbConnection');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+Connectdb();
+
+
+const corsOptions = {
+    origin: [
+        'http://localhost:3000', 
+        'https://contactvault202.netlify.app' 
+    ],
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server is running ✅");
+});
+
+app.get("/favicon.ico", (req, res) => res.status(204).end());
+
+app.use('/api/contact', require("./routes/contactRoutes"));
+app.use('/api/users', require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
